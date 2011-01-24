7856f24ccc19fbba3e8d75fd881eb90df830f890
ntxuan2401 <ntxuan2401@16c39d1a-15ea-2ab0-71af-d126500b71e6>
2011-01-24 04:05:36 +0000

    CompanyInfo da hoan thanh

--- trunk/Source/KimHoangDAO/CCompanyInfoDAO.cs
+++ trunk/Source/KimHoangDAO/CCompanyInfoDAO.cs
@@ -41,5 +41,15 @@ namespace KimHoangDAO
             return _provider.ExecuteUpdate("USP_ADMIN_UPD_COMPANY_INFO", _strName, _objValue);
         }
 
+        public int InsertCompanyInfo(CCompanyInfo arg_CompanyInfo, string arg_Crt_by)
+        {
+            if (arg_CompanyInfo == null)
+                return -1;
+            string[] _strName = new string[] { "@Id", "@Company_Name", "@Language_Id", "@Introduction_Info", "@Address", "@Address1", "@Address2", "@Tel", "@Tel1", "@Tel2", "@Email", "@Email1", "@Email2", "@Crt_By" };
+            object[] _objValue = new object[] { arg_CompanyInfo.Id, arg_CompanyInfo.Company_Name, arg_CompanyInfo.Language_Id, arg_CompanyInfo.Introduction_Info, arg_CompanyInfo.Address, arg_CompanyInfo.Address1, arg_CompanyInfo.Address2, arg_CompanyInfo.Tel, arg_CompanyInfo.Tel1, arg_CompanyInfo.Tel2, arg_CompanyInfo.Email, arg_CompanyInfo.Email1, arg_CompanyInfo.Email2, arg_Crt_by };
+            DataAccess _provider = new DataAccess();
+            return _provider.ExecuteUpdate("USP_ADMIN_UPD_COMPANY_INFO", _strName, _objValue);
+        }
+
     }
 }
